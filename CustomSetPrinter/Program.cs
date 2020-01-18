19d04541751130dd71db9fa972fb19cde672e073
ski lee <540470452@qq.com>
2020-01-18 16:37:26 +0800

    添加项目文件。

---
+++ CustomSetPrinter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomSetPrinterTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintDocument printDocument1 = new PrintDocument();
+            string defaultPrinterName = printDocument1.PrinterSettings.PrinterName; //默认打印机名称
+            if (string.IsNullOrEmpty(defaultPrinterName))
+            {
+                MessageBox.Show("未正确获取到默认打印机名称，请设置默认打印机！");
+                return;
+            }
+            string myPageSizeName = "MyCustomePageSize";
+            PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, myPageSizeName, 2000, 5000);
+            Console.ReadKey();
+        }
+    }
+}
