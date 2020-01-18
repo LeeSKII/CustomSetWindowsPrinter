78f3fc311b08f6a625c0acec764cb81da147152d
ski lee <540470452@qq.com>
2020-01-18 17:07:12 +0800

    增加异常处理和新增测试类型

--- CustomSetPrinter/Program.cs
+++ CustomSetPrinter/Program.cs
@@ -12,15 +12,28 @@ namespace CustomSetPrinterTest
     {
         static void Main(string[] args)
         {
-            PrintDocument printDocument1 = new PrintDocument();
-            string defaultPrinterName = printDocument1.PrinterSettings.PrinterName; //默认打印机名称
-            if (string.IsNullOrEmpty(defaultPrinterName))
+            try
             {
-                MessageBox.Show("未正确获取到默认打印机名称，请设置默认打印机！");
-                return;
+                PrintDocument printDocument1 = new PrintDocument();
+                string defaultPrinterName = printDocument1.PrinterSettings.PrinterName; //默认打印机名称
+                if (string.IsNullOrEmpty(defaultPrinterName))
+                {
+                    MessageBox.Show("未正确获取到默认打印机名称，请设置默认打印机！");
+                    return;
+                }
+                string myPageSizeName = "MyCustomePageSize";
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, myPageSizeName, 2000, 5000);
+
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.5A1", (float)5940.8, (float)12615);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.25A0", (float)8410, (float)14870);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.25A1", (float)5940.8, (float)10512.5);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "2.0A1", (float)5940.8, (float)16825);
             }
-            string myPageSizeName = "MyCustomePageSize";
-            PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, myPageSizeName, 2000, 5000);
+            catch (Exception ex)
+            {
+                MessageBox.Show("打印错误："+ex.Message);
+            }
+            
             Console.ReadKey();
         }
     }
