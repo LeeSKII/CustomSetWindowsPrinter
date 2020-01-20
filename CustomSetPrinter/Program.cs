a353462ed4297d9d24f20df0ef08d4cc9290757f
ski lee <540470452@qq.com>
2020-01-20 17:21:21 +0800

    增加图纸尺寸

--- CustomSetPrinter/Program.cs
+++ CustomSetPrinter/Program.cs
@@ -21,13 +21,45 @@ namespace CustomSetPrinterTest
                     MessageBox.Show("未正确获取到默认打印机名称，请设置默认打印机！");
                     return;
                 }
-                string myPageSizeName = "MyCustomePageSize";
-                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, myPageSizeName, 2000, 5000);
+                //string myPageSizeName = "MyCustomePageSize";
+                //PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, myPageSizeName, 2000, 5000);
+                #region 补全自定义纸张
+                //建议不要对系统内置的标准图纸进行操作
+                
+                //PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "A0", (float)8410.0, (float)11890.0);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.25A0", (float)8410.0, (float)14862.5);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.5A0", (float)8410.0, (float)17835.0);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.75A0", (float)8410.0, (float)20807.5);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "2.0A0", (float)8410.0, (float)23780.0);
 
-                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.5A1", (float)5940.8, (float)12615);
-                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.25A0", (float)8410, (float)14870);
-                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.25A1", (float)5940.8, (float)10512.5);
-                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "2.0A1", (float)5940.8, (float)16825);
+                //PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "A1", (float)5941.0, (float)8410.0);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.25A1", (float)5940.0, (float)10512.5);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.5A1", (float)5940.0, (float)12615.0);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.75A1", (float)5940.0, (float)14717.5);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "2.0A1", (float)5940.0, (float)16820.0);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "2.5A1", (float)5940.0, (float)21025);
+
+                //PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "A2", (float)4200.8, (float)5940.0);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.25A2", (float)4200.8, (float)7425);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.5A2", (float)4200.8, (float)8910);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.75A2", (float)4200.8, (float)10395);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "2.0A2", (float)4200.8, (float)11880);
+
+                //PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "A3", (float)2970, (float)4200);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.25A3", (float)2970, (float)5250);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.5A3", (float)2970, (float)6300);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.75A3", (float)2970, (float)7350);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "2.0A3", (float)2970, (float)8400);
+
+                //PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "A4", (float)2100, (float)2970);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.25A4", (float)2100, (float)3712.5);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.5A4", (float)2100, (float)4455);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "1.75A4", (float)2100, (float)5197.5);
+                PrinterPageSize.MyPageSize.SetPrintForm(defaultPrinterName, "2.0A4", (float)2100, (float)5940);
+
+
+                #endregion
+
             }
             catch (Exception ex)
             {
